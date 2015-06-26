2e060e5d6619a4a17bf5ffbdb378e3119cc783e3
Paul Anderson <paul@andersonloco.com>
2015-06-26 04:43:20 -0400

    Switching out to pack.

--- speaker.py
+++ speaker.py
@@ -18,8 +18,8 @@ DEV = 0  # Development mode. Change to zero for production.
 
 
 def show_pics():  # Load images and place on canvas.
-    i = 0  # Track rightness
-    j = imagetops  # Track downness.
+    #i = 0  # Track rightness
+    #j = imagetops  # Track downness.
 
     for file in glob.glob("images/*.jpg"):
         image = Image.open(file)
@@ -30,12 +30,9 @@ def show_pics():  # Load images and place on canvas.
         # label.image = photo
         # label.pack(side=LEFT)
         label.image = photo
-        label.grid(row=j, column=i)  # Move rightwise
-        i += 1
-        if i == maxwidth:
-            i = 0
-            j += 1
-    return j
+        label.pack()
+
+
 
 
 def show_instructions():
@@ -70,8 +67,7 @@ if __name__ == '__main__':
     # Are we in DEV mode? If so, show quit button.
     if DEV == 0:
         button = Button(frame, text="QUIT", fg="red", bg="blue", command=frame.quit)
-        button.grid(row=foo + 1,
-                    column=maxwidth / 2)  # show_pics()+1 ensures that quit button is below everything.
+        button.pack()
 
 
 
