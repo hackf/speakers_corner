a9134fc2ece442a084c61befca08f6816cfa119b
Paul Anderson <paul@andersonloco.com>
2015-06-26 02:59:31 -0400

    Moved display code into it's own method and changed directory images are read from.

--- speaker.py
+++ speaker.py
@@ -6,19 +6,26 @@ import glob
 
 size = 128,128  # Size of images to display
 
+#TODO Add Raspberry pi camera and GPIO code
+
 root=Tk()
 
 frame=Frame(root)
 frame.pack()
 
-for file in glob.glob("*.jpg"):
-    image=Image.open(file)
-    image.thumbnail(size,Image.ANTIALIAS)
-    photo=ImageTk.PhotoImage(image)
 
-    label=Label(image=photo)
-    label.image=photo
-    label.pack(side=LEFT)
+def show_pics():    #Load images and place on canvas.
+    for file in glob.glob("images/*.jpg"):
+        image = Image.open(file)
+        image.thumbnail(size, Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+
+        label = Label(image=photo)
+        label.image = photo
+        label.pack(side=LEFT)
+
+
+show_pics()
 
 button= Button(frame,text="QUIT", fg="red", command=frame.quit)
 button.pack(side=LEFT)
