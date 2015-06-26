902f74551b5fa9bdad0a779a93a6086a38a99f82
Paul Anderson <paul@andersonloco.com>
2015-06-26 04:55:06 -0400

    Having difficulty getting images positioned as I wish.

--- speaker.py
+++ speaker.py
@@ -12,14 +12,14 @@ size = 128, 128  # Size of images to display
 # frame = Frame(root, bg="blue")
 # frame.grid(row=0)
 
-#maxwidth = 5  # Sets the maximum number of columns if images in the window
+# maxwidth = 5  # Sets the maximum number of columns if images in the window
 
 DEV = 0  # Development mode. Change to zero for production.
 
 
 def show_pics():  # Load images and place on canvas.
-    #i = 0  # Track rightness
-    #j = imagetops  # Track downness.
+    # i = 0  # Track rightness
+    # j = imagetops  # Track downness.
 
     for file in glob.glob("images/*.jpg"):
         image = Image.open(file)
@@ -30,9 +30,7 @@ def show_pics():  # Load images and place on canvas.
         # label.image = photo
         # label.pack(side=LEFT)
         label.image = photo
-        label.pack(side=LEFT)
-
-
+        label.pack(fill=X,anchor=CENTER,side=LEFT)
 
 
 def show_instructions():
@@ -44,33 +42,31 @@ def show_instructions():
     inst = "Welcome to Speaker's Corner!\nPress button to begin recording!"
 
     # Create our own frame.
-    frame2=Frame(root,bg="blue")
-    frame2.pack(side=TOP,fill=BOTH, expand=1)
+    frame2 = Frame(root, bg="blue")
+    frame2.pack(side=TOP, fill=BOTH, expand=1)
     helv36 = tkFont.Font(family='Helvetica', size=36, weight="bold")
     label = Label(frame2, text=inst, bg="blue", font=helv36)
     label.pack()
 
 
 if __name__ == '__main__':
-    root=Tk()
-    #root.attributes("-bg", "blue")
+    root = Tk()
+    # root.attributes("-bg", "blue")
     # Fullscreen?
     if DEV == 0:
         root.attributes("-fullscreen", True)
     show_instructions()
 
     frame = Frame(root, bg="blue")
-    frame.pack(fill=BOTH,expand=1)
+    frame.pack(fill=BOTH, expand=1)
 
     foo = show_pics()
 
     # Are we in DEV mode? If so, show quit button.
     if DEV == 0:
-        #frame3=Frame(root,bg="blue")
-        #frame.pack(fill=BOTH,expand=1,side=BOTTOM)
+        # frame3=Frame(root,bg="blue")
+        # frame.pack(fill=BOTH,expand=1,side=BOTTOM)
         button = Button(frame, text="QUIT", fg="red", command=frame.quit)
         button.pack(side=BOTTOM)
-
-
 
 root.mainloop()
