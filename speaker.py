a66c9144288f97ca45994d167abf36b39e39f19c
Paul Anderson <paul@andersonloco.com>
2015-06-26 02:06:45 -0400

    Now capable of basic image display.

--- speaker.py
+++ speaker.py
@@ -2,6 +2,28 @@ __author__ = 'wackyvorlon'
 
 from Tkinter import *
 from PIL import ImageTk, Image
+import glob
+
+size = 128,128  # Size of images to display
+
+root=Tk()
+
+frame=Frame(root)
+frame.pack()
+
+for file in glob.glob("*.jpg"):
+    image=Image.open(file)
+    image.thumbnail(size,Image.ANTIALIAS)
+    photo=ImageTk.PhotoImage(image)
+
+    label=Label(image=photo)
+    label.image=photo
+    label.pack(side=LEFT)
+
+button= Button(frame,text="QUIT", fg="red", command=frame.quit)
+button.pack(side=LEFT)
+
+root.mainloop()
 
 
 
