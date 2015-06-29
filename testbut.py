0d7e51ae6e3cce4420d92f688816c7a3603f8f0e
Paul Anderson <paul@andersonloco.com>
2015-06-29 18:27:38 -0400

    Can now turn on LED.

--- testbut.py
+++ testbut.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python
 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 18
+
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.output(led, 1)
+time.sleep(5)
+GPIO.output(led, 0)
+
+GPIO.cleanup()
+
