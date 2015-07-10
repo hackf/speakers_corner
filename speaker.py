9ca18ab41b484dede160a1bc2808744a90eb7470
Paul Anderson <paul@andersonloco.com>
2015-07-10 00:18:08 -0400

    Changed close delay.

--- speaker.py
+++ speaker.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
     show_pics()
     setup_camera()
 
-    root.after(91000, frame.quit)
+    root.after(100000, frame.quit) # Delay before closing, dev use only
 
     if False:
         quitter()
