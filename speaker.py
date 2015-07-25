4f99ba0cdebd4297a03defca35adce3c18d9ead2
Paul Anderson <paul@andersonloco.com>
2015-07-25 08:02:35 -0400

    Some cleanup, also testing that cycle continues appropriately.

--- speaker.py
+++ speaker.py
@@ -57,67 +57,11 @@ def parsegeom(geometry):
     return map(int, m.groups())
 
 
-def show_pics():  # Load images and place on canvas.
-    i = 1  # Track rightness
-    label = []  # Our image labels
-
-    for fname in glob.glob("images/*.jpg"):
-        image = Image.open(fname)
-        image.thumbnail(size, Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-
-        label.append(Label(frame, image=photo, bg="blue"))
-        label[i - 1].image = photo
-        label[i - 1].grid(row=0, column=i)
-        i += 1
-
-    root.after(750, widther, label)  # Using Tkinter means that width numbers are 0 until
-                                     # screen is updated. So we wait 750ms.
 
 
-def widther(label):
-    """
-    Adds a dummy label to centre images.
-    :param label:
-    :return:
-    """
-
-    # Padding hack
-    # Find out how wide the labels are
-    q = 0
-    for widthulate in label:
-        q += widthulate.winfo_width()
-
-    # Grab window dimensions
-    windowsize = parsegeom(root.geometry())
-    width = windowsize[0]
-
-    # Calculate requisite padding
-    padding = (width - q) / 4  # Number found through experimentation. I have no idea why it's 4.
-    padding = abs(padding)     # In case too many images are in file, stop padding from going negative
-                               # and killing the program.
-
-    dummy = Label(frame, text=" ", bg="blue")
-    dummy.grid(row=0, column=0, padx=padding)
 
 
-def show_instructions():
-    """
-    Displays the instruction label.
 
-    :return:
-    """
-    # inst = "Welcome to Speaker's Corner!\nPress button to begin recording!"
-
-    with file("images/inst.txt") as f:
-        inst = f.read(100) # Maximum of 100 characters, more tends to fill the screen.
-
-    # Create our own frame.
-    frame2 = Frame(root, bg="blue")
-    frame2.pack(side=TOP, fill=BOTH, expand=1)
-    helv36 = tkFont.Font(family='Helvetica', size=36, weight="bold")
-    label = Label(frame2, text=inst, bg="blue", font=helv36, pady=200, fg="red")
-    label.pack()
 
 
 def setup_camera():
@@ -184,14 +128,13 @@ def change_image(im):
     size = parsegeom(root.geometry())
     sized = size[0], size[1]
     print sized
-    image.thumbnail(sized, Image.ANTIALIAS)
+    image.thumbnail(sized, Image.ANTIALIAS) # Resize to fit screen
     tkimage = ImageTk.PhotoImage(image)
     if back:
         back.pack_forget() # Remove previous label
     back = Label(root, image=tkimage)
     back.image = tkimage
     back.pack(fill=BOTH, expand=YES)
-    #back.place(x=0, y=0)
     root.after(10000,change_image,im)
 
 
@@ -208,18 +151,12 @@ if __name__ == '__main__':
     root.attributes("-fullscreen", True)
     root.configure(cursor='none')
 
-    #sponsor_background()
-
-    #show_instructions() # foo
-
-    #frame = Frame(root)
     root.after(500,sponsor_background)
-    #frame.pack(fill=BOTH, expand=YES)
-    #show_pics()
+
     #setup_camera()
 
 
-    root.after(30000, root.quit) # Delay before closing, dev use only
+    #root.after(30000, root.quit) # Delay before closing, dev use only
 
 
     root.mainloop()
