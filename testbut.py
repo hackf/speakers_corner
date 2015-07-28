8154433aaa21c08e317fa93bee32d2a65401e393
Paul Anderson <paul@andersonloco.com>
2015-07-28 16:20:11 -0400

    Begining to configure button.

--- testbut.py
+++ testbut.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
-import time
-import max7219.led as led
-from max7219.font import proportional, CP437_FONT
 
-device = led.matrix()
 
-device.show_message("Seconds left:", font=proportional(CP437_FONT))
-
-time.sleep(5)
-
-x=90
-
-while x>0:
-    device.show_message(str(x))
-    x-= 10
-    time.sleep(10)
+#while x>0:
+#    device.show_message(str(x))
+#    x-= 10
+#    time.sleep(10)
 
 
 
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-#led = 18
+button = 17
 
-#GPIO.setup(led, GPIO.OUT)
+GPIO.setup(button, GPIO.IN)
+
+while True:
+    if (GPIO.input(button)):
+        print "Pressed"
+
 
 #GPIO.output(led, 1)
 #time.sleep(5)
